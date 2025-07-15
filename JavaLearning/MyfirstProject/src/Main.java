67979fe257e8673f4372505ca59cf5d02a879942
gauravdeshmukh01 <gauravdeshmukh1010@gmail.com>
2025-07-15 23:24:14 +0530

    initial commit

---
+++ JavaLearning/MyfirstProject/src/Main.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+import java.util.Random;
+
+
+
+public class Main{
+
+
+
+    public static void main(String[] args) {
+
+//        System.out.println("Jay Shree Ram");
+//        System.out.println();
+//
+//        String email = "gauravdeshmukh1010@gmail.com";
+//
+//        int adi = 5;
+
+//        double pi = 3.14;
+//
+//        System.out.println("Your email is: " + email);
+
+       // Scanner scanner = new Scanner(System.in);
+
+//        System.out.print("Enter your name: ");
+//
+//        String name = scanner.nextLine(); // only .next for single name - Garry
+//
+//        int age = scanner.nextInt();  // after int or double always call scanner.nextLine(); to avoid input buffer
+//
+//        System.out.println("Hello " + name);
+
+//        int x = 2;
+//
+//        System.out.println(x++);  // prints 2 (then x becomes 3)
+//        System.out.println(++x);  // prints 4 (x becomes 4 before printing)
+
+
+        //   scanner.close();
+
+//
+//        double ans = 2 + 3 - 2.0 / (3*2) +7 ;
+//        System.out.println("helkli  " + ans);
+//
+//        while (1 == 1){
+//            System.out.println("Jay Shree Ram!");
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//        Random random = new Random();
+//
+//        int lowerBound = 1;
+//        int upperBound = 100;
+//        int numberToGuess = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+//        int guess;
+//        int attempts = 0;
+//
+//        System.out.println("🎮 Welcome to the Number Guessing Game!");
+//        System.out.println("Guess a number between " + lowerBound + " and " + upperBound);
+//
+//        while (true) {
+//            System.out.print("Enter your guess: ");
+//            guess = scanner.nextInt();
+//            attempts++;
+//
+//            if (guess == numberToGuess) {
+//                System.out.println("🎉 Correct! You guessed it in " + attempts + " attempts.");
+//                break;
+//            } else if (guess < numberToGuess) {
+//                System.out.println("Too low 📉. Try again.");
+//            } else {
+//                System.out.println("Too high 📈. Try again.");
+//            }
+//        }
+//
+//        scanner.close();
+
+
+      //  Scanner scanner = new Scanner(System.in);
+
+
+//        for(int i = 10; i > 0; i--){
+//            System.out.println(i);
+//            Thread.sleep(100);
+//        }
+//
+//        System.out.println("Ashadi Yekadashichya hardik shubhecha!");
+
+
+
+
+        }
+
+
+}
+
+
+
+
